ed1e0135183aabeb492b82b4a8c11408c45d74bb
Edgar S. Hurtado <edsanhu@gmail.com>
2025-03-29 22:53:08 +0100

    log user id

--- src/telegram_logger.py
+++ src/telegram_logger.py
@@ -51,7 +51,7 @@ class TelegramBot:
         if self.__repository.get_user_schedule_configuration(update.effective_user.id) is None:
             await context.bot.send_message(
                 chat_id=context.effective_chat.id,
-                text="You don't have power here!"
+                text="You don't have power here! Ask the master for permission\n\n Your id is:" + str(context.effective_user.id)
             )
             return
 
