7fb795b6e56f57ddbbd9e3940ecace7bcf9b9d24
Edgar S. Hurtado <edsanhu@gmail.com>
2025-03-30 09:26:41 +0200

    Remove outdated jobs from json

--- src/repository.py
+++ src/repository.py
@@ -1,6 +1,9 @@
 import copy
 import json
 import os
+from datetime import datetime, timedelta
+
+from src.box_data import days_in_advance
 
 
 class JsonRepository:
@@ -19,6 +22,11 @@ class JsonRepository:
     def add_booking_to_user(self, user_id, booking_goal):
         user_schedule_data = self.__find_user_schedule_configuration(user_id)
         already_registered_booking = self.__find_booking_schedule(user_id, booking_goal)
+        already_registered_booking_datetime = datetime.strptime(already_registered_booking['datetime'], '%d-%m-%Y %H:%M')
+        if already_registered_booking and  already_registered_booking_datetime < (datetime.now() + timedelta(days=days_in_advance)):
+            self.delete_booking_from_user(user_id, already_registered_booking['job_id'])
+            return
+
         if already_registered_booking is not None:
             already_registered_booking.update({'job_id': booking_goal['job_id']})
         else:
