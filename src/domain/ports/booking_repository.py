3d0efe3543b6b90ef557c263a6e8944921e35e94
Edgar S. Hurtado <edsanhu@gmail.com>
2026-08-31 15:16:21 +0200

    Fix typo

--- src/domain/ports/booking_repository.py
+++ src/domain/ports/booking_repository.py
@@ -4,7 +4,7 @@ from domain.models import BookingGoal, BookingSchedule
 
 class IBookingRepository(ABC):
     @abstractmethod
-    def get_user_bookings(self, user_id: int) -> BookingSchedule:
+    def get_user_bookings(self, user_id: int) -> BookingSchedule: ...
 
     @abstractmethod
     def add_booking_goal(self, user_id: int, goal: BookingGoal) -> None: ...
